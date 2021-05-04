827f9326dfab43f80db939df76cb898ccea0f33e
persongu <83482358+persongu@users.noreply.github.com>
2021-05-04 00:40:19 -0700

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,7 +11,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Helllo World!" );
     }
 
     private void foo() throws Exception {
