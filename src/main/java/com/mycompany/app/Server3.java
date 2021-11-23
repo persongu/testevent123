0dfa57a01d09fd5cb1282b006ab7ed039299cc44
Gus Person <persongu@amazon.com>
2021-11-23 14:20:27 -0800

    Test Case -> delete entire function
    Testing a case where the entire function containing the bug is deleted.  Points should NOT be awarded.

--- src/main/java/com/mycompany/app/Server3.java
+++ src/main/java/com/mycompany/app/Server3.java
@@ -13,15 +13,6 @@ public class Server3
     {
         System.out.println( "Hello World!" );
     }
-
-    private void foo(String p) {
-       String s = "aloha";
-       System.out.println(String.format("This is a faulty message: %i", s));
-       System.out.format("No %s",1);
-       if ("1" == "2") {
-            //dosomething
-       }
-    }
   
     private void cipher(String s) throws Exception
     {
